3b71739578c3bf5ea61a3049d9cbd7644d9a4a3e
mirabellensaft <tanja@transfeld.net>
2017-08-03 14:08:34 +0200

    added comments

--- svgTOcssElefant.py
+++ svgTOcssElefant.py
@@ -20,36 +20,53 @@ class Coordinates( xml.sax.ContentHandler ):
 
 
         if tag == "path":
+
+            # only if the # is in the line, the path belongs to a polygon
             style = attributes["style"]
 
+            # polygon_id is the hexadecimal code of the stroke
+            # polygons of the same id are different animation stages of the same polygon
+            polygon_id = style[54:61]
+            background = style[5:12]
 
+            # parsing the coordinates
             content = attributes["d"]
+
+            # to make css-coordinates from the svg-path, characters other then
+            # numbers and commas have to be omitted
             for char in content:
                 if char in numbers:
                     raw_coordinates += char
 
+
         raw_coordinates = raw_coordinates.replace(',', ' ')
         Liste = raw_coordinates.split()
 
+        # the pixel number is reduced to 3 places and used as%
         for i in range(len(Liste)):
             if i%2 == 0:
                 coordinates += Liste[i][0:4] + "% "
             else:
                 coordinates += Liste[i][0:4] + "%, "
 
+        # ....to get rid of the final comma
         coordinates = coordinates[0:-2]
 
         Platzhalter = style[5:12]+"replace"
 
+        # wrapping css
+        # first line is a comment to identify the polygon
+        target.write("/*object{}*/\n".format(polygon_id))
         target.write("    100% {{\n")
-        target.write("      background: {};\n".format(style[5:12]))
+        target.write("      background: {};\n".format(background))
         target.write("      clip-path: polygon({});\n" .format(coordinates))
         target.write("    }\n")
         target.write("  }\n")
         target.write("\n")
 
-
 print("Creator for coordinates for the End of the Animation")
+
+# file operations
 
 filename_target = input("Name the final file: ")
 target = open(filename_target, "w")
@@ -61,7 +78,6 @@ filename_origin_svg = input("Name of the .svg file, containing path data:  ")
 origin_svg = open(filename_origin_svg, "r")
 
 
-
 if ( __name__ == "__main__"):
     parser = xml.sax.make_parser()
     parser.setFeature(xml.sax.handler.feature_namespaces, 0)
@@ -69,8 +85,6 @@ if ( __name__ == "__main__"):
     parser.setContentHandler(Handler)
 
     parser.parse(origin_svg)
-
-
 
 origin_svg.close()
 origin_css.close()
